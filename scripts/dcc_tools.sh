bc929b3c2ea3a4ee9439109da9ed6d88e46c9e46
Dung-Ru Tsai <you@example.com>
2024-05-06 15:03:37 -0700

    init/prepare/apply/build

--- scripts/dcc_tools.sh
+++ scripts/dcc_tools.sh
@@ -7,19 +7,21 @@
 # Email: octobersky.tw@gmail.com
 
 # Initialize variables for option flags and parameters
-source_value=0
+init_value=0
 prepare_value=0
+apply_value=0
 build_value=0
 untar_value=0
-restore_pkg_value=0
+clean_up_value=0
 # Define an associative array where the key is the source tarball and the value is the target directory
 # Only put the Original DCC Release tarball that we care
 declare -A tarballs=(
     ["mt7915_20221209-a9012a.tar.xz"]="mt7915_mt_wifi"
     ["mt79xx_20221209-b9c02f.tar.xz"]="mt_wifi"
 )
-# Directory 
-BASE_DIR="mtk-wifi-mt79xx/dl"
+# Directory
+SDK_BASE_DIR="mtk-wifi-mt79xx"
+DL_BASE_DIR="mtk-wifi-mt79xx/dl"
 PACKAGE_BASE_DIR="mtk-wifi-mt79xx/package"
 # Official DCC Release Note Commit
 OPENWRT_COMMIT="295c612"
@@ -33,13 +35,14 @@ BUILD_ARGUMENT="5g"
 
 # Function to display usage information
 usage() {
-    echo "Usage: $(basename $0) [-s|--source] [-p|--prepare] [-b|--build]"
-    echo "  -s, --get_source      Get openwrt source code and checkout"
-    echo "  -p, --prepare         Prepare environment"
+    echo "Usage: $(basename $0) [-i|--init] [-p|--prepare] [-a|--apply] [-b|--build]"
+    echo "  -i, --init            Init the build Env and get openwrt source code and checkout"
+    echo "  -p, --prepare         Tar DL source directories into their respective new tarballs, and Modify the PKG_SOURCE"
+    echo "  -a, --apply           COPY the SDK into the openwrt source from SDK folder"
     echo "  -b, --build           Build project"
+    echo "  -c, --clean_up        Clean up DCC tarballs and Restore the PKG_SOURCE to original DCC release version"
     echo "  --untar               Untar DL source tarball to target directory"
     echo "  --tar                 Tar DL source directories into their respective tarballs"
-    echo "  --restore_pkg         Restore the PKG_SOURCE to original DCC release version"
     exit 1
 }
 
@@ -49,12 +52,12 @@ untar() {
     local target_dir="$2"
 
     echo "Untarring $source_tarball to $target_dir"
-    mkdir -p "$BASE_DIR/$target_dir"
-    tar -xvf "$BASE_DIR/$source_tarball" -C "$BASE_DIR/$target_dir" --strip-components=1
+    mkdir -p "$DL_BASE_DIR/$target_dir"
+    tar --exclude-vcs -xvf "$DL_BASE_DIR/$source_tarball" -C "$DL_BASE_DIR/$target_dir"
 }
 
 # Function to tar a new tarball and change the new PKG_SOURCE
-apply_new_tarball() {
+update_new_pkg_source() {
     local base_tarball_name=$1
     local new_tarball_name=$2
     # Search for the Makefile path by base_tarball_name and replace PKG_SOURCE with new_tarball_name
@@ -75,8 +78,8 @@ tar_dir() {
     local tarball_extension="${source_tarball##*.}" # Get the file extension
     local new_tarball_name="${base_tarball_name}-${head_commit}.tar.${tarball_extension}"
     echo "Tarring $target_dir into $new_tarball_name"
-    tar -cvf "$BASE_DIR/$new_tarball_name" -C "$BASE_DIR/$target_dir" .
-    apply_new_tarball $base_tarball_name $new_tarball_name
+    tar -cJf "$DL_BASE_DIR/$new_tarball_name" -C "$DL_BASE_DIR/$target_dir" .
+    update_new_pkg_source $base_tarball_name $new_tarball_name
 }
 
 # Get Openwrt and source options
@@ -111,7 +114,7 @@ build_1st_time() {
 }
 
 # Restore Package
-restore_pkg() {
+clean_up() {
     local target_dir=$1
     local source_tarball=$2
     local base_tarball_name="${source_tarball%%.*}" # Remove the file extension
@@ -119,6 +122,7 @@ restore_pkg() {
     if [[ -n "$makefile_path" ]]; then
         sed -i "s/^PKG_SOURCE:=$base_tarball_name.*/PKG_SOURCE:=$source_tarball/" "$makefile_path"
         echo "Updated PKG_SOURCE in $makefile_path to $source_tarball"
+        find $SDK_BASE_DIR -name "$base_tarball_name*-dcc-*tar.xz" -exec rm {} \;
     else
         echo "Makefile not found for $source_tarball"
     fi
@@ -134,10 +138,13 @@ while [[ "$#" -gt 0 ]]; do
     case "$1" in
 
         -s|--source)
-            source_value=1
+            init_value=1
             ;;
         -p|--prepare)
             prepare_value=1
+            ;;
+        -a|--apply)
+            apply_value=1
             ;;
         -b|--build)
             build_value=1
@@ -148,8 +155,8 @@ while [[ "$#" -gt 0 ]]; do
         --tar)
             tar_value=1
             ;;
-        --restore_pkg)
-            restore_pkg_value=1
+        -c|--clean_up)
+            clean_up_value=1
             ;;
         *)
             # If an unknown option is provided, display usage information
@@ -160,35 +167,46 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Implement the actions based on the flags and parameters
-if [ "${source_value}" -eq 1 ]; then
-    echo "Option -s/--source was triggered"
+if [[ "${init_value}" -eq 1 ]]; then
+    echo "Option -i/--init was triggered"
     get_source
 fi
 
-if [ "${prepare_value}" -eq 1 ]; then
+if [[ "${prepare_value}" -eq 1 ]]; then
     echo "Option -p/--prepare was triggered"
-    prepare_feeds
-fi
-
-if [ "${build_value}" -eq 1 ]; then
-    echo "Option -b/--build was triggered"
-    build_1st_time
-fi
-
-if [ "${untar_value}" -eq 1 ]; then
-    for source_tarball in "${!tarballs[@]}"; do
-        untar "$source_tarball" "${tarballs[$source_tarball]}"
-    done
-fi
-
-if [ "${tar_value}" -eq 1 ]; then
     for source_tarball in "${!tarballs[@]}"; do
         tar_dir "${tarballs[$source_tarball]}" "$source_tarball"
     done
 fi
 
-if [ "${restore_pkg_value}" -eq 1 ]; then
+if [[ "${apply_value}" -eq 1 ]]; then
+    echo "Option -a/--apply was triggered"
+    rsync -av $SDK_BASE_DIR/ openwrt/
     for source_tarball in "${!tarballs[@]}"; do
-        restore_pkg "${tarballs[$source_tarball]}" "$source_tarball"
+        rm -rf "openwrt/dl/${tarballs[$source_tarball]}"
+    done
+    prepare_feeds
+fi
+
+if [[ "${clean_up_value}" -eq 1 ]]; then
+    for source_tarball in "${!tarballs[@]}"; do
+        clean_up "${tarballs[$source_tarball]}" "$source_tarball"
+    done
+fi
+
+if [[ "${build_value}" -eq 1 ]]; then
+    echo "Option -b/--build was triggered"
+    build_1st_time
+fi
+
+if [[ "${untar_value}" -eq 1 ]]; then
+    for source_tarball in "${!tarballs[@]}"; do
+        untar "$source_tarball" "${tarballs[$source_tarball]}"
+    done
+fi
+
+if [[ "${tar_value}" -eq 1 ]]; then
+    for source_tarball in "${!tarballs[@]}"; do
+        tar_dir "${tarballs[$source_tarball]}" "$source_tarball"
     done
 fi
